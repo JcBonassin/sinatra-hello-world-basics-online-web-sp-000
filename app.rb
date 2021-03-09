cf4fe3423ad571474d9e630d8af56e9bac68e4ef
JC <bonassin@gmail.com>
2021-03-09 15:54:35 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
   get '/' do
-          "hey man bueno bueno"
-      end     
+          "Hello, World!"
+      end
 
 end
